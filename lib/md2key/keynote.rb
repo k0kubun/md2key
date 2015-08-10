57db380da752ee834dcc32a36c3465f30055d2eb
bin liu <liubin0329@gmail.com>
2015-08-10 14:39:01 +0800

    convert img src in markdown to full pass to support relative image file path

--- lib/md2key/keynote.rb
+++ lib/md2key/keynote.rb
@@ -30,7 +30,7 @@ module Md2key
 
       # Insert image to the last slide
       def insert_image(path)
-        execute_applescript('insert_image', slides_count, path, TEMPLATE_SLIDE_INDEX)
+        execute_applescript('insert_image', slides_count, File.absolute_path(path), TEMPLATE_SLIDE_INDEX)
       end
 
       def insert_code(code)
