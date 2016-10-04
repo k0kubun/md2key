d4c0fc20c8fd4a503c6164817526c2e4955664e1
Takashi Kokubun <takashikkbn@gmail.com>
2016-10-05 00:33:36 +0900

    Allow changing master slide

--- lib/md2key/converter.rb
+++ lib/md2key/converter.rb
@@ -25,13 +25,14 @@ module Md2key
       cover_master = Keynote.fetch_master_slide_name(1)
       main_master  = Keynote.fetch_master_slide_name(2)
 
-      Keynote.update_cover(markdown.cover, cover_master)
+      Keynote.update_cover(markdown.cover, config.cover_master || cover_master)
       markdown.slides.each do |slide|
+        master = config.slide_master(slide.level) || main_master
         if slide.table
-          Keynote.create_slide_with_table(slide, slide.table.rows, slide.table.columns, main_master)
+          Keynote.create_slide_with_table(slide, slide.table.rows, slide.table.columns, master)
           Keynote.insert_table(slide.table.data)
         else
-          Keynote.create_slide(slide, main_master)
+          Keynote.create_slide(slide, master)
           Keynote.insert_image(slide.image) if slide.image
           Keynote.insert_code(slide.code) if slide.code
         end
--- lib/md2key/slide.rb
+++ lib/md2key/slide.rb
@@ -1,5 +1,5 @@
 module Md2key
-  class Slide < Struct.new(:title, :lines, :image, :code, :table, :note)
+  class Slide < Struct.new(:title, :lines, :image, :code, :table, :note, :level)
     def initialize(*)
       super
       self.lines ||= []
